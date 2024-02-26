d04dcf83badddc2c3fb30bf12d4dd25e9d4b5376
Ruben <ruben.melo@finkargo.com>
2024-02-26 18:43:36 -0500

    activation class and implementation

--- neuron_object.py
+++ neuron_object.py
@@ -1,10 +1,16 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
 
-np.random.seed(0)
+nnfs.init()
+
+#np.random.seed(0)
 
 X = [[1, 2, 3, 2.5],
      [2, 5, -1, 2],
      [-1.5, 2.7, 3.3, -0.8]]
+
+X, y = spiral_data(100, 3)
 
 #inputs deben normalizarse entre -1 y 1
 #weights debe normalizarse entre -0.1 y 0.1
@@ -16,10 +22,16 @@ class Layer_Dense:
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
-layer1 = Layer_Dense(4, 5)
-layer2 = Layer_Dense(5, 2)# tiene que ser del shape de la capa anterior
+class Activation_ReLU():
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
 
+layer1 = Layer_Dense(2, 5)
+#layer2 = Layer_Dense(5, 2)# tiene que ser del shape de la capa anterior
+activation1 = Activation_ReLU()
 layer1.forward(X)
 print(layer1.output)
-layer2.forward(layer1.output)
-print(layer2.output)
+activation1.forward(layer1.output)
+print(activation1.output)
+#layer2.forward(layer1.output)
+#print(layer2.output)
